dd662433d053d89c328b3fbc999dd7a27f4c0309
Haidahazmi <haidahazmi@gmail.com>
2026-06-14 21:36:31 +0800

    Add files via upload

---
+++ lib/Domain/samsactivityclaimmodel.dart
@@ -0,0 +1,13 @@
+class SAMSActivityClaimModel {
+  String claimId;
+  String studentId;
+  String activityName;
+  String status;
+
+  SAMSActivityClaimModel({
+    required this.claimId,
+    required this.studentId,
+    required this.activityName,
+    required this.status,
+  });
+}
