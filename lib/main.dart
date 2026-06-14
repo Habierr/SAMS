beb4878127e4bd010b1c0ab0821198691284f0d8
Hanimazmi <hanimazmi77@gmail.com>
2026-06-14 21:12:20 +0800

    Add lecturer and student dashboard routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
-import 'Provider/ORController.dart';
-import 'Provider/sams_financial_controller.dart';
+
 import 'firebase_options.dart';
 import 'login.dart';
 
+import 'package:sams/Page/lecturer/lecturer_dashboard.dart';
+import 'package:sams/Page/lecturer/attendanceManagement.dart';
+import 'package:sams/Page/lecturer/generateClassCode.dart';
+import 'package:sams/Page/lecturer/classCodeList.dart';
+import 'package:sams/Page/student/student_dashboard.dart';
+
+import 'Provider/ORController.dart';
+import 'Provider/sams_financial_controller.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  Map<String, dynamic> _getArgs(BuildContext context) {
+    return ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,6 +42,94 @@ class MyApp extends StatelessWidget {
         title: 'Faculty Registrar - SAMS',
         debugShowCheckedModeBanner: false,
         home: const Login(),
+        routes: {
+          '/login': (context) => const Login(),
+          '/faculty_dashboard': (context) => const FacultyDashboard(),
+          '/lecturer_dashboard': (context) => const LecturerDashboard(
+                lecturerName: '',
+                lecturerId: '',
+              ),
+          '/pusat_dashboard': (context) => const PusatDashboard(),
+          '/student_dashboard': (context) => const StudentDashboard(
+                studentName: '',
+                studentId: '',
+                year: '',
+                semester: '',
+              ),
+          '/treasury_dashboard': (context) => const TreasuryDashboard(),
+
+          '/attendanceManagement': (context) {
+            final args = _getArgs(context);
+
+            return AttendanceManagement(
+              lecturerName: args['lecturerName'] ?? '',
+              lecturerId: args['lecturerId'] ?? '',
+              regID: args['regID'] ?? '',
+              subCode: args['subCode'] ?? '',
+              subName: args['subName'] ?? '',
+              classType: args['classType'] ?? '',
+              sectionNo: args['sectionNo'] ?? '',
+              day: args['day'] ?? '',
+              startTime: args['startTime'] ?? '',
+              endTime: args['endTime'] ?? '',
+              semester: args['semester'] ?? '',
+            );
+          },
+
+          '/generateClassCode': (context) {
+            final args = _getArgs(context);
+
+            return GenerateClassCode(
+              lecturerName: args['lecturerName'] ?? '',
+              lecturerId: args['lecturerId'] ?? '',
+              regID: args['regID'] ?? '',
+              subCode: args['subCode'] ?? '',
+              subName: args['subName'] ?? '',
+              classType: args['classType'] ?? '',
+              sectionNo: args['sectionNo'] ?? '',
+              day: args['day'] ?? '',
+              startTime: args['startTime'] ?? '',
+              endTime: args['endTime'] ?? '',
+              semester: args['semester'] ?? '',
+            );
+          },
+
+          '/classCodeList': (context) {
+            final args = _getArgs(context);
+
+            return ClassCodeList(
+              lecturerName: args['lecturerName'] ?? '',
+              lecturerId: args['lecturerId'] ?? '',
+              regID: args['regID'] ?? '',
+              subCode: args['subCode'] ?? '',
+              subName: args['subName'] ?? '',
+              classType: args['classType'] ?? '',
+              sectionNo: args['sectionNo'] ?? '',
+              day: args['day'] ?? '',
+              startTime: args['startTime'] ?? '',
+              endTime: args['endTime'] ?? '',
+              semester: args['semester'] ?? '',
+            );
+          },
+
+          '/attendanceRecord': (context) {
+            final args = _getArgs(context);
+
+            return AttendanceRecord(
+              lecturerName: args['lecturerName'] ?? '',
+              lecturerId: args['lecturerId'] ?? '',
+              regID: args['regID'] ?? '',
+              subCode: args['subCode'] ?? '',
+              subName: args['subName'] ?? '',
+              classType: args['classType'] ?? '',
+              sectionNo: args['sectionNo'] ?? '',
+              day: args['day'] ?? '',
+              startTime: args['startTime'] ?? '',
+              endTime: args['endTime'] ?? '',
+              semester: args['semester'] ?? '',
+            );
+          },
+        },
       ),
     );
   }
