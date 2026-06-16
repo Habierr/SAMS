f5e54fced537dac7b32130c85a18744e7f3c8e15
Haidahazmi <haidahazmi@gmail.com>
2026-06-16 14:06:40 +0800

    Update samsactivityclaimmodel.dart

--- lib/Domain/samsactivityclaimmodel.dart
+++ lib/Domain/samsactivityclaimmodel.dart
@@ -1,9 +1,20 @@
+// Model class for Curriculum Activity Claim
 class SAMSActivityClaimModel {
+
+  // Unique ID for each activity claim
   String claimId;
+
+  // Student matric number
   String studentId;
+
+  // Name of curriculum activity
   String activityName;
+
+  // Current claim status
+  // Example: PENDING, APPROVED, REJECTED
   String status;
 
+  // Constructor to initialize activity claim object
   SAMSActivityClaimModel({
     required this.claimId,
     required this.studentId,
